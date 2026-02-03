9648136cbc1d65e346ca671827188cdcb23f930a
Akshay Patnaik <akshaykpatnaik06@gmail.com>
2026-02-03 13:08:15 -0700

    Initial Commit

---
+++ BoringPhoneInterface/BoringPhoneInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoringPhoneInterfaceApp.swift
+//  BoringPhoneInterface
+//
+//  Created by Akshay Patnaik on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BoringPhoneInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoringPhoneInterfaceTests/BoringPhoneInterfaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoringPhoneInterfaceTests.swift
+//  BoringPhoneInterfaceTests
+//
+//  Created by Akshay Patnaik on 2/3/26.
+//
+
+import Testing
+@testable import BoringPhoneInterface
+
+struct BoringPhoneInterfaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
